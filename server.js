b92c82a5c5c55461a0744bcd24a74ee41598d6d1
joko triyanto <jokotriyanto123@outlook.com>
2021-12-22 07:58:46 +0700

    rename ip variable

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/whoami", (req, res) => {
-  const ip = req.ip;
+  const ipaddress = req.ip;
   const language = req.headers['accept-language'];
   const software = req.headers['user-agent'];
-  res.json({ip, language, software});
+  res.json({ipaddress, language, software});
 });
 
 // listen for requests :)
